ab9741bf127eb67a3cb316c34101b78730a63cec
CereSan <70066768+CereSan@users.noreply.github.com>
2020-08-23 15:47:41 +0700

    Fix MultiScore, Add level
    initial init

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const tanah = document.querySelectorAll('.tanah');
-const tikus = document.querySelectorAll('.tikus');
-const papanSkor = document.querySelector('.papan-skor');
-const pop = document.querySelector('#pop');
+const tanah = document.querySelectorAll(".tanah");
+const tikus = document.querySelectorAll(".tikus");
+const papanSkor = document.querySelector(".papan-skor");
+const pop = document.querySelector("#pop");
 
 let tanahSebelumnya;
 let selesai;
 let skor;
+let i;
 
 function randomTanah(tanah) {
   const t = Math.floor(Math.random() * tanah.length);
@@ -21,36 +22,49 @@ function randomWaktu(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-function munculkanTikus() {
+function munculkanTikus(min, max) {
+  i++;
+  console.log(i);
   const tRandom = randomTanah(tanah);
-  const wRandom = randomWaktu(300, 1000);
-  tRandom.classList.add('muncul');
-
+  const wRandom = randomWaktu(min, max);
+  tRandom.classList.add("muncul");
+  tambahEvent(); // Menambah eventListener penambah score
   setTimeout(() => {
-    tRandom.classList.remove('muncul');
+    tRandom.classList.remove("muncul");
     if (!selesai) {
-      munculkanTikus();
+      munculkanTikus(min, max);
     }
   }, wRandom);
 }
 
-function mulai() {
+function mulai(min, max) {
+  i = 0;
+  console.log("Min: " + min + "|| Max: " + max);
   selesai = false;
   skor = 0;
   papanSkor.textContent = 0;
-  munculkanTikus();
+  munculkanTikus(min, max);
   setTimeout(() => {
     selesai = true;
   }, 10000);
 }
 
 function pukul() {
+  hapusEvent(); // Hapus eventListener penambah score
   skor++;
-  this.parentNode.classList.remove('muncul');
+  this.parentNode.classList.remove("muncul");
   pop.play();
   papanSkor.textContent = skor;
 }
 
-tikus.forEach(t => {
-  t.addEventListener('click', pukul);
-});
+function tambahEvent() {
+  tikus.forEach((t) => {
+    t.addEventListener("click", pukul);
+  });
+}
+
+function hapusEvent() {
+  tikus.forEach((t) => {
+    t.removeEventListener("click", pukul);
+  });
+}
